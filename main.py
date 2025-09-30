f12a04e787ad1fd325d73ec728a0922d5d793ef4
Achilova-arch <asalachilova96@gmail.com>
2025-09-30 10:08:53 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,81 @@
+# Variant (2)
+# Nazariy vazifalar (50 ball)
+# 1. print() - bu nima? U nima vazifa bajaradi?
+# print bu  bizning kodlarimizni chiqarish uchun foydalanadi
+
+
+# 2. input() - haqida ma'lumot yozing. U nima vazifa bajaradi?
+# input bu bizga qanaqadur malumat yoki so'z larni chiqarishga yordam beradi
+
+
+# 3. Bir ma'lumot turini boshqa turga o'tkazish uchun nima qilish kerak?
+# boshqa malumot turini o'tgazish uchun o'zgartiramiz
+
+
+# 4. Math metodlari haqida ma'lumot yozing, kamida 5ta.
+# sqrt,pow,floor,ceil,raund
+
+
+
+# 5. if, elif, else - bular nima? Ular nima vazifa bajaradi?
+# if ga biza savol yozamiz va bu savol javobi true bolsa  qanaqadir shart qoyamiz
+# elif ga if ga ohshash lekin biz savol yozmimz bizashartni o'zini yozamiz
+# else esa javobi false bolsa qanaqadur ish qiladi
+
+
+# Baholash va Kriterialar
+# Har bir to’liq to’g’ri javob uchun - 10 ball
+# To’liq emas, lekin qisman to’g’ri javob uchun - 5 ball
+# Noto’g’ri javob uchun - 0 ball
+# Eslatma: Agar chatgpt yoki shunga o’xshash manbaalardan ko’chirilgani sezilsa 0 ball qo’yilsin
+# Umumiy: 50 ball
+# Amaliy vazifalar (50 ball)
+
+# 6. "I like apples" satrida "apples" so'zini "bananas" so'ziga almashtiring va chop eting.
+# output: I like bananas
+# f="I like banas"
+
+
+
+# 7. "Hello, world!" satrining 7 dan 12-gacha bo'lgan qismini oling va chop eting.
+# output: world
+# d="Hello, world"
+# print(d[7:12])
+
+
+# 8. input() orqali kiritilgan sonning ildiz ostini aniqlovchi dastur tuzing
+# input: number = 25
+# output: 25 sonining ildiz osti = 5.0
+# import math
+# son=input("Son kiriting")
+# square = math.sqrt(son)
+# print(square)
+
+
+
+
+# 9. Foydalanuvchi yoshini kiriting va uni bolalar, o'smirlar va kattalar sifatida tasniflang. 13 yoshdan kichiklar bola, 13 va 18 yosh oraliqg’idagilar o’smir va 18yoshdan kattalar katta yoshli bo’lishi kerak
+# input: yosh = 15
+# # output: Siz o’smir yoshdasiz
+# yosh=input("Yoshingizni kiriting: ")
+# if yosh in range(13,18):
+#     print("Siz o'smir yoshdasiz")
+# else:
+#     print("Sizning yoshingiz katta")
+
+
+
+# 10. Foydalanuvchi matn kiritishini so'ran. Kiritilgan matndagi harflar, probellar, simvollar va raqamlar soni aniqlansin
+# input: "Python - bu 1-raqamli dasturlash tili. U 1991-yilda yaratilgan."
+# output: Harflar: 47, Probellar: 8, Simvollar: 5, Raqamlar: 5
+# matn=input("matin kiriting:")
+# print(len(matn))
+# print(matn.isdigit())
+
+
+# Baxolash va kriteriyalari
+# Har bir to’liq to’g’ri javob uchun - 10 ball
+# To’liq emas, lekin qisman to’g’ri javob uchun - 5 ball
+# Noto’g’ri javob uchun - 0 ball
+# Eslatma: Agar chatgpt yoki shunga o’xshash manbaalardan ko’chirilgani sezilsa 0 ball qo’yilsin
+# Umumiy: 50 ball
